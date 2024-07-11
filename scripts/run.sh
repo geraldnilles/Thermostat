f99dd496aa08338403231236e36484d399b77360
Gerald Nilles <gerald@nilleye.com>
2024-07-11 16:49:44 -0700

    Switch Run.sh to use the python version

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,38 +4,5 @@ set -e
 
 cd "$(dirname "$0")"
 
-. paths.sh
-
-
-mkdir -p $RUN_DIR
-
-###################################
-# Configure the runtime temp files
-###################################
-
-# If no mode exists, set it to off.sh
-if [ ! -f $RUN_DIR/mode.sh ]
-then
-	echo Setting Default mode to off
-	ln -s $PWD/off_mode.sh $RUN_DIR/mode.sh
-fi
-
-# If no offset exists, set it to zero
-if [ ! -f $RUN_DIR/offset.txt ]
-then
-	echo Setting Default offset to zero
-	echo 0 > $RUN_DIR/offset.txt
-fi
-
-# If no timer exists, set it to zero
-if [ ! -f $RUN_DIR/timer.txt ]
-then
-	echo Setting Default offset to zero
-	echo 0 > $RUN_DIR/timer.txt
-fi
-
-
-$RUN_DIR/mode.sh
-
-./plot.py
+python3 ./run.py
 
